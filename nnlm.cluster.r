61e92b241fa34920e49e77835683e6d125b5c292
MingyuYang-Yale <50332096+MingyuYang-Yale@users.noreply.github.com>
2019-11-04 16:39:00 -0500

    Delete nnlm.cluster.r

--- nnlm.cluster.r
+++
@@ -1,6 +0,0 @@
-library("NNLM")
-data <- read.table("0725cL-50um-e10.selected.tsv",header=T)
-normdata=log(sweep(t(data),2,colSums(t(data)),"/")*10000+1)
-nmf.results = nnmf(t(normdata),11)
-write.table(nmf.results$W,file="0725cL-50um-e10.W-11.selected.norm.tsv",sep="\t")
-write.table(t(nmf.results$H),file="0725cL-50um-e10.Ht-11.selected.norm.tsv",sep="\t")
